5c7904f17b69adea992f956a5ce69357b2954b57
sethlagman <sethglagman@gmail.com>
2026-02-28 22:47:28 +0800

    Update allowed_hosts and debug to be .env configurable

--- hoopcentral/hoopcentral/settings.py
+++ hoopcentral/hoopcentral/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h.strip() for h in os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',') if h.strip()]
 
 
 # Application definition
