24f5ddd7c7a76482ad72c492f2488ffd3bd81fda
sethlagman <sethglagman@gmail.com>
2026-05-01 02:57:59 +0800

    Update to store the latest is_active field
    Takes "SEASON" from env to determine which season to retrieve is_active

--- backend/data_ingestion/processer/process_nba_player.py
+++ backend/data_ingestion/processer/process_nba_player.py
@@ -1,22 +1,17 @@
 import json
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+SEASON = (os.environ.get('SEASON') or '2025-26').strip()
 RAW_DIR = BASE_DIR / 'output' / 'raw'
 PROCESSED_DIR = BASE_DIR / 'output' / 'processed'
 PLAYER_HEADSHOTS = 'https://cdn.nba.com/headshots/nba/latest/1040x760/'
 
-def get_nba_player_image_url(player_id: str, url=PLAYER_HEADSHOTS) -> str:
-    """
-    Returns API image url for a player's headshot
+load_dotenv(BASE_DIR.parent.parent / '.env')
 
-    :param player_id: The id of a player
-    :type player_id: str
-    :param url: API Url for requesting player headshot pictures
-    :return: Returns a string url for a player's headshot
-    :rtype: str
-    """
-
+def get_nba_player_image_url(player_id: str, url: str = PLAYER_HEADSHOTS) -> str:
     return f'{url}{player_id}.png'
 
 
@@ -33,41 +28,37 @@ def process_nba_player(raw_file=RAW_DIR / 'nba_player_data.json'):
 
     total_statistics = []
     for year, data in player_data.items():
-
-        statistics = {
-            year: []
-        }
-
+        stats_for_year = []
         for player in data['resultSets'][0]['rowSet']:
-            stat_dict = {
-                'id': player[0],
-                'ppg': player[20],
-                'rpg': player[21],
-                'apg': player[22],
-            }
+            stats_for_year.append(
+                {
+                    'id': player[0],
+                    'team_id': player[4],
+                    'ppg': player[20],
+                    'rpg': player[21],
+                    'apg': player[22],
+                }
+            )
+        total_statistics.append({year: stats_for_year})
 
-            statistics[year].append(stat_dict)
+    profile_season = SEASON if SEASON in player_data else list(player_data.keys())[-1]
 
-        total_statistics.append(statistics)
-
-    latest_year = list(player_data.keys())[-1]
     players = []
-
-    for player in player_data[latest_year]['resultSets'][0]['rowSet']:
-        player_dict = {
-            'id': player[0],
-            'full_name': f'{player[2]} {player[1]}',
-            'last_name': player[1],
-            'first_name': player[2],
-            'team_id': player[4],
-            'jersey': player[10],
-            'is_active': 'true' if player[19] is not None else 'false',
-            'headshot': get_nba_player_image_url(player_id=player[0]),
-            'year_start': player[24],
-            'year_end': player[25],
-        }
-
-        players.append(player_dict)
+    for player in player_data[profile_season]['resultSets'][0]['rowSet']:
+        players.append(
+            {
+                'id': player[0],
+                'full_name': f'{player[2]} {player[1]}',
+                'last_name': player[1],
+                'first_name': player[2],
+                'team_id': player[4],
+                'jersey': player[10],
+                'is_active': 'true' if player[19] is not None else 'false',
+                'headshot': get_nba_player_image_url(player_id=player[0]),
+                'year_start': player[24],
+                'year_end': player[25],
+            }
+        )
 
     with open(PROCESSED_DIR / 'nba_player.json', 'w') as f:
         json.dump(players, f, indent=2)
